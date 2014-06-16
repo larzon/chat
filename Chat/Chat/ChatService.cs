f7e3095be41e0013f40b7fdb36007e5d65afc434
larzon <lana.arzon@gmail.com>
2014-06-16 23:49:53 +0200

    Initial commit

---
+++ Chat/Chat/ChatService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+namespace Chat {
+  public partial class ChatService : ServiceBase {
+    public ChatService() {
+      InitializeComponent();
+    }
+
+    protected override void OnStart(string[] args) {
+      BinaryServerFormatterSinkProvider provider = new BinaryServerFormatterSinkProvider(); 
+      provider.TypeFilterLevel = System.Runtime.Serialization.Formatters.TypeFilterLevel.Full; 
+      IDictionary props = new Hashtable(); props["port"] = 12345; 
+      TcpChannel chan = new TcpChannel(props, null, provider); 
+      ChannelServices.RegisterChannel(chan); 
+      RemotingConfiguration.RegisterWellKnownServiceType(typeof(ChatServer), "Chat.ChatServer", WellKnownObjectMode.Singleton);
+    }
+
+    protected override void OnStop() {
+    }
+  }
+
+  public class ChatClient : MarshalByRefObject { 
+    public void Reci(string s) { 
+      Console.WriteLine(s); 
+    } 
+  }
+
+  public class ChatServer : MarshalByRefObject { 
+    List<ChatClient> lc = new List<ChatClient>(); 
+    
+    public void DodajKlijenta(ChatClient c) { 
+      lc.Add(c); 
+    } 
+    
+    public void ReciSvima(ChatClient c, string s) { 
+      for (int i = 0; i < lc.Count; ++i) { 
+        try { 
+          if (lc[i] != c) lc[i].Reci(s); 
+        } 
+        catch { 
+          lc.RemoveAt(i); 
+          --i; 
+        } 
+      } 
+    } 
+  }
+}
---
+++ ChatCli/ChatCli/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using Chat;
+
+namespace ChatCli {
+  class Program {
+    static void Main(string[] args) {
+      ChatClient ja = new ChatClient(); 
+      TcpChannel chan = new TcpChannel(0); 
+      ChannelServices.RegisterChannel(chan); 
+      ChatServer server = (ChatServer)Activator.GetObject(typeof(ChatServer), @"tcp://localhost:12345/Chat.ChatServer"); 
+      if (server == null) { 
+        System.Console.WriteLine("Nema servera"); 
+        return; 
+      } 
+      server.DodajKlijenta(ja); 
+      while (true) { 
+        string s = Console.ReadLine(); 
+        if (s == "") break; 
+        server.ReciSvima(ja, s); 
+      }
+    }
+  }
+}
