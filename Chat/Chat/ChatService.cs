5d9f66a9b45c4c5cf051d6f19028177c8546ce7e
akovacic <in.math0@gmail.com>
2014-06-26 15:34:05 +0200

    Još dodavanje komentara

--- Chat/Chat/ChatService.cs
+++ Chat/Chat/ChatService.cs
@@ -13,17 +13,17 @@ using System.Runtime.Remoting.Channels.Tcp;
 
 namespace Chat {
   public partial class ChatService : ServiceBase {
-    public ChatService() {
+    public ChatService() {//ostvarivanje windows servisa
       InitializeComponent();
     }
 
     protected override void OnStart(string[] args) {
       BinaryServerFormatterSinkProvider provider = new BinaryServerFormatterSinkProvider(); 
       provider.TypeFilterLevel = System.Runtime.Serialization.Formatters.TypeFilterLevel.Full; 
-      IDictionary props = new Hashtable(); props["port"] = 12345; 
+      IDictionary props = new Hashtable(); props["port"] = 12345; //server će slušati a portu 12345
       TcpChannel channel = new TcpChannel(props, null, provider); 
-      ChannelServices.RegisterChannel(channel); 
-      RemotingConfiguration.RegisterWellKnownServiceType(typeof(ChatServer), "Chat.ChatServer", WellKnownObjectMode.Singleton);
+      ChannelServices.RegisterChannel(channel); //registriranje na kanal
+      RemotingConfiguration.RegisterWellKnownServiceType(typeof(ChatServer), "Chat.ChatServer", WellKnownObjectMode.Singleton);//ime servera je "Chat.Chatserver"
     }
 
     protected override void OnStop() {
@@ -33,11 +33,11 @@ namespace Chat {
   public class ChatClient : MarshalByRefObject {
     public string username;
 
-    public ChatClient(string username) {
+    public ChatClient(string username) {//referenca na klijenta
       this.username = username;
     }
 
-    public void Send(string message, string senderUsername = null) {
+    public void Send(string message, string senderUsername = null) {//klijent šalje poruku, server je ispisuje na ostale klijente
       if (senderUsername == null) Console.WriteLine(message);
       else Console.WriteLine(senderUsername + ": " + message); 
     } 
@@ -48,7 +48,7 @@ namespace Chat {
 
     public bool UserExists(string username) {
       try {
-        if (clientList.Exists(client => client.username == username)) return true;
+        if (clientList.Exists(client => client.username == username)) return true;//provjeravanje postoji li klijent u grupi za chat, ako da, sve ok, ako ne, znači da je klijent napustio chat
         return false;
       }
       catch {
@@ -57,20 +57,20 @@ namespace Chat {
     }
     
     public void AddClient(ChatClient client) {
-      clientList.Add(client); 
+      clientList.Add(client); //dodavanje nekog klijenta u listu klijenata (za chat)
     }
 
     public void RemoveClientAt(int i) {
-      clientList.RemoveAt(i);
+      clientList.RemoveAt(i);//brisanje klijenta iz liste klijenata
     }
     
     public void SendToAll(ChatClient client, string message) {
       for (int i = 0; i < clientList.Count; ++i) { 
         try {
-          if (clientList[i] != client) clientList[i].Send(message, client.username); 
+          if (clientList[i] != client) clientList[i].Send(message, client.username);//slanje poruke klijentu j od klijenta i ako j!=i 
         } 
         catch {
-          clientList.RemoveAt(i); 
+          clientList.RemoveAt(i); //ovaj klijent je odstupio od chatanja 
           --i; 
         } 
       } 
@@ -79,7 +79,7 @@ namespace Chat {
     public void SendNotification(string message, ChatClient client) {
       for (int i = 0; i < clientList.Count; ++i) {
         try {
-          if (clientList[i] != client) clientList[i].Send(message);
+          if (clientList[i] != client) clientList[i].Send(message);//slanje notifikacija klijentima -> da se neki drugi klijent pridružio ili izišao iz razgovora
         }
         catch {
           clientList.RemoveAt(i);
